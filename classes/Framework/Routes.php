f53768d124267902f2b00363859c18b32bd79171
Konrad008 <kontakt@konradalbrecht.pl>
2018-03-04 20:48:14 +0100

    Pierwsza faza boilerplejta

---
+++ classes/Framework/Routes.php
@@ -0,0 +1,6 @@
+<?php
+namespace Framework;
+
+interface Routes {
+    public function getRoutes();
+}
---
+++ templates/layout.html.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css">
+    <link rel="stylesheet" href="/assets/css/milligram.min.css">
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <title><?=$title?></title>
+</head>
+<body>
+<nav>
+    <header>
+        <h1>Internet Joke Database</h1>
+    </header>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/joke/list">Jokes List</a></li>
+        <li><a href="/joke/edit">Add a new Joke</a></li>
+    </ul>
+</nav>
+
+<main>
+    <?=$output?>
+</main>
+
+<footer>
+    <span style="color: #4cc5f1">blue</span>note Authentication Manager 2018
+</footer>
+</body>
+</html>q
